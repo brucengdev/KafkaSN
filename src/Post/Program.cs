d23a6302ac5cc3a8d14123fb5cb1347b7ac87e71
brucengdev <brucengdev@gmail.com>
2025-12-30 13:40:19 +0700

    Added DI to use rabbitmq or kafka in Post service.

--- src/Post/Program.cs
+++ src/Post/Program.cs
@@ -1,23 +1,42 @@
 using Post;
 using Confluent.Kafka;
 using QueueClient;
+using Microsoft.Extensions.Options;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
-builder.Services.AddKafkaConsumerConfig(builder.Configuration);
-builder.Services.AddSingleton<IEventConsumer>(services =>
+
+var queueToUse = builder.Configuration["QUEUE"];
+if(queueToUse == "rabbitmq")
 {
-    var config = services.GetRequiredService<ConsumerConfig>();
-    var consumer = new KafkaEventConsumer(config);
-    consumer.Subscribe("createPost");
+    builder.Services.AddRabbitMQConfig(builder.Configuration);
+    builder.Services.AddSingleton<IEventConsumer>(services =>
+    {
+        var config = services.GetRequiredService<IOptions<RabbitMQConfig>>();
+        var consumer = new RabbitMQEventConsumer(config);
+        consumer.Subscribe("createPost").GetAwaiter().GetResult();
 
-    return consumer;
-});
+        return consumer;
+    });
+
+    builder.Services.AddSingleton<IEventProducer, RabbitMQEventProducer>();
+} else {
+    builder.Services.AddKafkaConsumerConfig(builder.Configuration);
+    builder.Services.AddSingleton<IEventConsumer>(services =>
+    {
+        var config = services.GetRequiredService<ConsumerConfig>();
+        var consumer = new KafkaEventConsumer(config);
+        consumer.Subscribe("createPost").GetAwaiter().GetResult();
+
+        return consumer;
+    });
 
 
-builder.Services.AddKafkaProducerConfig(builder.Configuration);
-builder.Services.AddSingleton<IEventProducer, KafkaProducer>();
+    builder.Services.AddKafkaProducerConfig(builder.Configuration);
+    builder.Services.AddSingleton<IEventProducer, KafkaProducer>();
+}
+
 
 var host = builder.Build();
 host.Run();
