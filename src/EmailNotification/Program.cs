1d8aedbc1753127815c5552415e2a5ffa5a6379d
brucengdev <brucengdev@gmail.com>
2025-12-30 13:42:11 +0700

    Implement for EmailNotification service.

--- src/EmailNotification/Program.cs
+++ src/EmailNotification/Program.cs
@@ -1,19 +1,35 @@
 using Confluent.Kafka;
 using  EmailNotification;
+using Microsoft.Extensions.Options;
 using QueueClient;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
 
-builder.Services.AddKafkaConsumerConfig(builder.Configuration);
-builder.Services.AddSingleton<IEventConsumer>(services =>
-{
-    var config = services.GetRequiredService<ConsumerConfig>();
-    var consumer = new KafkaEventConsumer(config);
-    consumer.Subscribe(["accountCreated", "postCreated"]);
 
-    return consumer;
+var queueToUse = builder.Configuration["QUEUE"];
+if(queueToUse == "rabbitmq")
+{
+    builder.Services.AddRabbitMQConfig(builder.Configuration);
+    builder.Services.AddSingleton<IEventConsumer>(services =>
+    {
+        var config = services.GetRequiredService<IOptions<RabbitMQConfig>>();
+        var consumer = new RabbitMQEventConsumer(config);
+        consumer.Subscribe(["accountCreated", "postCreated"]).GetAwaiter().GetResult();
+
+        return consumer;
+    });
+} else {
+    builder.Services.AddKafkaConsumerConfig(builder.Configuration);
+    builder.Services.AddSingleton<IEventConsumer>(services =>
+    {
+        var config = services.GetRequiredService<ConsumerConfig>();
+        var consumer = new KafkaEventConsumer(config);
+        consumer.Subscribe(["accountCreated", "postCreated"]).GetAwaiter().GetResult();
+
+        return consumer;
 });
+}
 
 var host = builder.Build();
 host.Run();
